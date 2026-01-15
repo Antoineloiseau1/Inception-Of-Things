2bfd4a4ee5ec0fab6bc931f72ad26c46ac6a8b1b
madem23 <manon.demma@gmail.com>
2026-01-15 16:08:33 +0100

    adding files for P3 not working yet, + repo available in GITHUB at madem23/IoT-mdemma-ncardozo-anloisea

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -2,8 +2,8 @@
 
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip 192.168.56.110
 
-kubectl apply -f /vagrant/apps/app1.yaml
-kubectl apply -f /vagrant/apps/app2.yaml
-kubectl apply -f /vagrant/apps/app3.yaml
-kubectl apply -f /vagrant/apps/default-ingress.yaml
-kubectl apply -f /vagrant/apps/ingress.yaml
+kubectl apply -f /vagrant/confs/app1.yaml
+kubectl apply -f /vagrant/confs/app2.yaml
+kubectl apply -f /vagrant/confs/app3.yaml
+kubectl apply -f /vagrant/confs/default-ingress.yaml
+kubectl apply -f /vagrant/confs/ingress.yaml
