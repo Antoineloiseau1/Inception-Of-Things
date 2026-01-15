5d3d7f9dcf294f9bedd35007fb64851124715c1e
anloisea <anloisea@Iot.student.42.com>
2026-01-15 11:22:24 +0100

    struggling to make app3 default

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -4,3 +4,8 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip 192
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
 sudo chmod 644 /vagrant/node-token
+
+kubectl apply -f /vagrant/apps/app1.yaml
+kubectl apply -f /vagrant/apps/app2.yaml
+kubectl apply -f /vagrant/apps/app3.yaml
+kubectl apply -f /vagrant/apps/ingress.yaml
