fe4d6bbef245edaa040ce2cef3d242acbfe7c3d4
anloisea <anloisea@Iot.student.42.com>
2026-02-04 14:14:22 +0100

    working

--- bonus/scripts/setup-cluster.sh
+++ bonus/scripts/setup-cluster.sh
@@ -1,14 +1,23 @@
+#!/bin/bash
+
+# Colors
+BLUE="\033[1;34m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "🔹 Setting Up k3d cluster"
+echo -e "${YELLOW}Setting Up k3d cluster${RESET}"
 if k3d cluster list | grep -q "iot"; then
     echo "K3d cluster "iot" already exists"
 else
     echo "Creating k3d cluster..."
-    k3d cluster create "iot" --wait -p "8888:8888@loadbalancer" -p "443:443@loadbalancer"
+    k3d cluster create "iot" --wait -p "8888:8888@loadbalancer"
 fi
 
-echo "Creating namespaces..."
+echo -e "${YELLOW}\nCreating namespaces...${RESET}"
 for ns in "argocd" "dev" "gitlab"; do
     if ! kubectl get ns "$ns" >/dev/null 2>&1; then
         kubectl create ns $ns
@@ -17,43 +26,43 @@ for ns in "argocd" "dev" "gitlab"; do
     fi
 done
 
-echo "Installing Argo CD..."
+echo -e "${YELLOW}\nInstalling Argo CD...${RESET}"
 if ! kubectl get deployment argocd-server -n argocd >/dev/null 2>&1; then
   kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 else
   echo "Argo CD already installed, skipping"
 fi
 
-echo "🔹 Waiting for Argo CD to be ready"
+echo -e "${YELLOW}\nWaiting for Argo CD to be ready${RESET}"
 kubectl rollout status deployment argocd-server -n argocd
 
 
-if ! helm repo list | grep -q "gitlab"; then                                                          
-      helm repo add gitlab http://charts.gitlab.io/                                                     
-  else                                                                                                  
-      echo "GitLab repo already added"                                                                  
-fi 
+if ! helm repo list | grep -q "gitlab"; then
+      helm repo add gitlab http://charts.gitlab.io/
+  else
+      echo "GitLab repo already added"
+fi
 
-echo "🔹 Installing GitLab 🔹"
+echo -e "${YELLOW}\nInstalling GitLab...${RESET}"
 helm upgrade --install gitlab gitlab/gitlab \
     --namespace gitlab \
     --wait \
     --timeout 600s \
     -f ${SCRIPT_DIR}/../confs/gitlab-values.yaml
 
+kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8181 >/dev/null 2>&1 &
 bash ${SCRIPT_DIR}/seed-repo.sh
-
 kubectl apply -f ${SCRIPT_DIR}/../confs/app.yaml
 
 GITLAB_PASSWORD=$(kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d && echo)
 echo
-echo "Password to access GitLab: ${GITLAB_PASSWORD}"
+echo -e "${BLUE}Password to access GitLab: ${GITLAB_PASSWORD}${RESET}"
 echo
 
-kubectl -n argocd patch configmap argocd-cm --type merge -p '{"data":{"url":"https://argocd.localhost"}}'
+kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
 ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo
-echo "Argo CD initial admin password: ${ARGOCD_PASSWORD}"
+echo -e "${BLUE}Argo CD initial admin password: ${ARGOCD_PASSWORD}${RESET}"
 echo
 
-echo "Setup complete!"
+echo -e "${GREEN}\nSetup complete!\n${RESET}"
--- p3/scripts/setup-cluster.sh
+++ p3/scripts/setup-cluster.sh
@@ -39,9 +39,8 @@ kubectl apply -f ${SCRIPT_DIR}/../confs
 
 echo -e "${GREEN}\nSetup complete!\n${RESET}"
 
-echo -e "${BLUE}To access ArgoCD's GUI, go to http://localhost:8080, login = admin, and enter the following password:${RESET}"
+echo -e "${BLUE}To access ArgoCD's GUI, go to https://localhost:8080, login = admin, and enter the following password:${RESET}"
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
 echo
-
-kubectl port-forward svc/argocd-server -n argocd 8080:443
